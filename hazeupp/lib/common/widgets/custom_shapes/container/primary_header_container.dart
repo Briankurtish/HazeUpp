717b81cd6e9feb38edc33e873712d68204448cef
Annon_101 <67450280+Briankurtish@users.noreply.github.com>
2024-06-17 13:04:46 +0300

    created custom widget and curved widget

---
+++ hazeupp/lib/common/widgets/custom_shapes/container/primary_header_container.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:hazeupp/common/widgets/custom_shapes/container/circular_container.dart';
+import 'package:hazeupp/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:hazeupp/utils/constants/colors.dart';
+
+class THeaderPrimaryContainer extends StatelessWidget {
+  const THeaderPrimaryContainer({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return TCurvedEdgeWidget(
+      child: Container(
+        color: TColors.primary,
+        padding: const EdgeInsets.all(0),
+        child: SizedBox(
+          height: 400,
+          child: Stack(
+            children: [
+              Positioned(
+                top: -150,
+                right: -250,
+                child: TCircularConatiner(
+                  backgroundColor: TColors.textWhite.withOpacity(0.1),
+                ),
+              ),
+              Positioned(
+                top: 100,
+                right: -300,
+                child: TCircularConatiner(
+                  backgroundColor: TColors.textWhite.withOpacity(0.1),
+                ),
+              ),
+              child,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- hazeupp/lib/features/shop/screens/home/widgets/home.dart
+++ hazeupp/lib/features/shop/screens/home/widgets/home.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:hazeupp/common/widgets/custom_shapes/container/circular_container.dart';
+import 'package:hazeupp/common/widgets/custom_shapes/container/primary_header_container.dart';
+import 'package:hazeupp/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:hazeupp/utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            THeaderPrimaryContainer(
+              child: Container(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
