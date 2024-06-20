6eb1d5d44da0a948440b87282408dca93edbfdd6
Annon_101 <67450280+Briankurtish@users.noreply.github.com>
2024-06-20 19:57:37 +0300

    firebase configuration initial

--- hazeupp/lib/features/shop/screens/brands/all_brands.dart
+++ hazeupp/lib/features/shop/screens/brands/all_brands.dart
@@ -3,11 +3,9 @@ import 'package:get/get.dart';
 import 'package:hazeupp/common/widgets/appbar/appbar.dart';
 import 'package:hazeupp/common/widgets/layouts/grid_layout.dart';
 import 'package:hazeupp/common/widgets/products/product_cards/brand_card.dart';
-import 'package:hazeupp/common/widgets/products/sortable/t_sortable_products.dart';
 import 'package:hazeupp/common/widgets/texts/section_heading.dart';
 import 'package:hazeupp/features/shop/screens/brands/brand_products.dart';
 import 'package:hazeupp/utils/constants/sizes.dart';
-import 'package:iconsax/iconsax.dart';
 
 class AllBrandsScreen extends StatelessWidget {
   const AllBrandsScreen({super.key});
@@ -24,23 +22,6 @@ class AllBrandsScreen extends StatelessWidget {
           padding: const EdgeInsets.all(TSizes.defaultSpace),
           child: Column(
             children: [
-              // Drop Down
-              // DropdownButtonFormField(
-              //   decoration: InputDecoration(
-              //     prefixIcon: Icon(Iconsax.sort),
-              //   ),
-              //   onChanged: (value) {},
-              //   items: [
-              //     "Name",
-              //     "Higher Price",
-              //     "Lower Price",
-              //     "Newest",
-              //     "Popularity"
-              //   ]
-              //       .map((option) =>
-              //           DropdownMenuItem(value: option, child: Text(option)))
-              //       .toList(),
-              // ),
               const SizedBox(height: TSizes.spaceBtwSections),
               // Heading
               TSectionHeading(title: "Brands", showActionButton: false),
--- hazeupp/lib/main.dart
+++ hazeupp/lib/main.dart
@@ -1,14 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:hazeupp/app.dart';
+import 'package:hazeupp/firebase_options.dart';
 
+// Entry point of flutter app
+Future<void> main() async {
+  // Todo: Add widgets binding
+  // Todo: Init Local Storage
+  // Todo: Init payment methods
+  // Todo: Await Native Splash
+  // Todo: Initialise firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then(
+    (FirebaseApp value) => Get.to(AuthenticationRepository()),
+  );
+  // Todo: Initialise Authentication
 
-void main() {
+  // Load all the material design themes / localizations / bindings
   runApp(const App());
 }
-
-
-
-
-
-
-
